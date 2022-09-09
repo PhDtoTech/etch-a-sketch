ca6641458e7d63d2aaf2eb6169253ca6e6e523b2
Paulina Ramirez <paulina.rn@gmail.com>
2022-09-09 17:23:49 -0500

    Add 16 x 16 grid using js loop and CSS grid

--- eas.js
+++ eas.js
@@ -1,6 +1,6 @@
 const grid = document.getElementById('grid');
 
-for(let i=0; i<9; i++) {
+for(let i=0; i<256; i++) {
     const square = document.createElement('div');
     square.className = "square";
     grid.appendChild(square);
