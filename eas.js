e62fcc986b152f2cd3528af60629288453f222cf
Paulina Ramirez <paulina.rn@gmail.com>
2022-09-15 08:09:09 -0500

    Add reset functionality

--- eas.js
+++ eas.js
@@ -1,21 +1,20 @@
+// declares variable with element #grid
 const grid = document.getElementById('grid');
 
+//creates 256 boxes through loop by declaring box variable, adding class "square", and appending it to grid
 for(let i=0; i<256; i++) {
     const box = document.createElement('div');
-    box.className = "square";
+    box.classList.add('square');
     grid.appendChild(box);
 }
 
+//declares variable with nodelist of elements including "square" class
 const squares = document.querySelectorAll('.square');
 
+//adds class "hover" when mouse hovers elements to each element in nodelist
 squares.forEach(square => {
   square.addEventListener('mouseover', () => {
     square.classList.add('hover');
   });
 });
 
-
-/*box.addEventListener('mouseover', () => {
-    square.style.backgroundColor = "black";
-    square.classList.add('hover');
-  });*/
