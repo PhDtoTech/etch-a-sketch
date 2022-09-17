e84131657f96cb573e1b3f156b37806c7552d8f9
Paulina Ramirez <paulina.rn@gmail.com>
2022-09-17 17:21:01 -0500

    Add rainbow button functionality

--- eas.js
+++ eas.js
@@ -1,27 +1,91 @@
-// declares variable with element #grid
+// declare variable with element #grid
 const grid = document.getElementById('grid');
 
+<<<<<<< Updated upstream
 //creates 256 boxes through loop by declaring box variable, adding class "square", and appending it to grid
 for(let i=0; i<256; i++) {
     const box = document.createElement('div');
     box.classList.add('square');
     grid.appendChild(box);
+=======
+// declare variable with button #density
+const densityButton = document.getElementById('density');
+
+//generate random color
+const getRandomNumber = (maxNum) => {
+  return Math.floor(Math.random() * maxNum);
+};
+ 
+const getRandomColor = () => {
+  const h = getRandomNumber(360);
+  const s = getRandomNumber(100);
+  const l = getRandomNumber(100);
+  return `hsl(${h}deg, ${s}%, ${l}%)`;
+};
+
+//declare variable that represents number of squares per side of grid
+let sideLength = 16;
+
+// get number of squares per side of grid
+function getDensity(){
+  sideLength = (prompt("How many squares per side?"))*1;
+  return sideLength;
 }
 
-//declares variable with nodelist of elements including "square" class
+// add event listener on click and runs function
+densityButton.addEventListener('click', () => {
+  getDensity();
+});
+
+//create boxes based on sideLength through loop by declaring box variable, adding class "square", and appending it to grid
+for(let i=0; i<(sideLength*sideLength); i++) {
+  const box = document.createElement('div');
+  box.classList.add('square');
+  grid.appendChild(box);
+>>>>>>> Stashed changes
+}
+
+//declare variable with original color value
+let color = '#aac179';
+
+//add reset function
+function resetGrid() {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.className = 'square';
+    square.style.backgroundColor= 'white';
+    color = '#aac179'
+  });
+}
+
+//declares variable with reset button
+const reset = document.getElementById('reset');
+
+//adds on click event to reset button
+reset.addEventListener('click', () => {
+  resetGrid();
+});
+
+//declare variable with nodelist of elements including "square" class
 const squares = document.querySelectorAll('.square');
 
-//adds class "hover" when mouse hovers elements to each element in nodelist
+//add class "hover" when mouse hovers elements to each element in nodelist
 squares.forEach(square => {
   square.addEventListener('mouseover', () => {
-    square.classList.add('hover');
+    square.style.backgroundColor = color;
   });
 });
 
-//removes class hover when clicking reset button
-squares.forEach(square => {
-  const reset=document.getElementById('reset');
-  reset.addEventListener('click', () => {
-    square.classList.remove('hover');
-  });
+//pick random color when clicking rainbow button
+const rainbowButton = document.getElementById('rainbow');
+rainbowButton.addEventListener('click', () => {
+  resetGrid();
+  const randomColor = getRandomColor();
+  color = randomColor;
+});
+
+//pick original green when clicking green button
+const greenButton = document.getElementById('green');
+greenButton.addEventListener('click', () => {
+    resetGrid();
 });
